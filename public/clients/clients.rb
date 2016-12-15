3dfed2fd637f6cea5da3e253f2d4b19a902b8497
Colin Arpe <colin.j.arpe@outlook.com>
2016-12-15 17:22:31 -0500

    Create ruby script to gather file paths of client assets for Portfolio page

---
+++ public/clients/clients.rb
@@ -0,0 +1,43 @@
+client_folders = Dir.glob("*/")
+
+client_array = []
+
+client_folders.each_with_index do |folder, i|
+		client_array[i] = next_client(folder)
+end
+
+def next_client |client|
+	this_client = {folder: client, name: "", text: "", ad: "", logo: ""}	# declare new empty hash, add folder name
+
+	Dir.chdir("./" + this_client[:folder])	# enter client file, retrieve file names
+	client_assets = Dir.glob
+
+	client_name_file = File.open(client_assets[2], "r")	# read client name and text
+	this_client[:name] = client_name_file.read
+	client_text_file = File.open(client_assets[3], "r")
+	this_client[:text] = client_text_file.read
+
+	this_client[:ad] = "clients/" + client + client_assets[0]	#create image paths
+	this_client[:logo] = "clients/" + client + client_assets[1]
+
+	this_client
+end
+
+
+# def Client (folder_name)
+# 	@folder = folder_name
+# 	@assets = get_assets
+# 	@name = ""
+# 	@text = ""
+# 	@ad = ""
+# 	@logo = ""
+# 	attr_reader :name, :text, :ad, :logo
+
+# 	def get_assets
+# 		D
+# 	end
+
+# 	def get_name
+		
+# 	end
+# end
