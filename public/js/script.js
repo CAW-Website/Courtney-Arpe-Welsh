de568116ebc93796aa48913367c2991eb8c09f2d
Colin Arpe <colin.j.arpe@outlook.com>
2016-12-17 04:43:00 -0500

    Implement JS to make company profile appear on carousel click

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,11 @@
 
 $(document).ready (function () {
-
+	for (let i = 0; i < $(".client-ad-image").length; i++) {
+		$(".client-ad-image").eq(i).on ("click", function() {
+			for (var j = 0; j < $(".client-profile").length; j++) {
+				$(".client-profile").eq(j).css ("display", "none");
+			}
+			$(".client-profile").eq(i).css ("display", "block");
+		});
+	}
 });
